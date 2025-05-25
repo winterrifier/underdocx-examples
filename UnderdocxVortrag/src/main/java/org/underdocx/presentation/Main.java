ad182d9b70d2a8fd9eb2dbb89c1f0c4cef61a956
Gerald Winter <github@winterium.de>
2025-05-25 19:39:16 +0200

    keep formatting of placeholders that are not executed yet

--- UnderdocxVortrag/src/main/java/org/underdocx/presentation/Main.java
+++ UnderdocxVortrag/src/main/java/org/underdocx/presentation/Main.java
@@ -2,11 +2,13 @@ package org.underdocx.presentation;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.underdocx.common.cli.UnderdocxEngineRunner;
+import org.underdocx.environment.UnderdocxEnv;
 import org.underdocx.presentation.extensions.Presentation;
 
 
 public class Main {
     public static void main(String[] args) {
+        UnderdocxEnv.getInstance().tryNotToScanIgnoredNodes = true;
         String engineProviderName = Presentation.class.getName();
         String[] newArgs = ArrayUtils.addFirst(args, engineProviderName);
         UnderdocxEngineRunner.main(newArgs);
