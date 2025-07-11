e16ed254f464304969506d599d7ff2f9475dd72c
Gerald Winter <github@winterium.de>
2025-07-11 17:26:19 +0200

    Added generation with debug mode

--- UnderdocxVortrag/src/main/java/org/underdocx/presentation/Main.java
+++ UnderdocxVortrag/src/main/java/org/underdocx/presentation/Main.java
@@ -5,12 +5,18 @@ import org.underdocx.common.cli.UnderdocxEngineRunner;
 import org.underdocx.environment.UnderdocxEnv;
 import org.underdocx.presentation.extensions.Presentation;
 
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
+        boolean debug = args.length > 0 && args[0].equals("debug");
         UnderdocxEnv.getInstance().tryNotToScanIgnoredNodes = true;
+        UnderdocxEnv.getInstance().isDebug = debug;
         String engineProviderName = Presentation.class.getName();
-        String[] newArgs = ArrayUtils.addFirst(args, engineProviderName);
+        String[] newArgs = !debug
+                ? ArrayUtils.addFirst(args, engineProviderName)
+                : ArrayUtils.addFirst(Arrays.copyOfRange(args, 1, args.length), engineProviderName);
         UnderdocxEngineRunner.main(newArgs);
         System.exit(0);
     }
