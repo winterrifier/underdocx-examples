939324a73c93b5e9f99491611331e407bab66111
Gerald Winter <github@winterium.de>
2025-07-17 07:17:50 +0200

    fixes after review

--- UnderdocxVortrag/presentation/step.bat
+++ UnderdocxVortrag/presentation/step.bat
@@ -13,22 +13,6 @@ if exist generated03.odp (
     set inputFile=generated03.odp
     set outputFile=generated04.odp
 )
-if exist generated04.odp (
-    set inputFile=generated04.odp
-    set outputFile=generated05.odp
-)
-if exist generated05.odp (
-    set inputFile=generated05.odp
-    set outputFile=generated06.odp
-)
-if exist generated06.odp (
-    set inputFile=generated06.odp
-    set outputFile=generated07.odp
-)
-if exist generated07.odp (
-    set inputFile=generated07.odp
-    set outputFile=generated08.odp
-)
 
 java -jar underdocx-vortrag-0.1-jar-with-dependencies.jar ./%inputFile% ./%outputFile% ./presentationData.json
 start "" ./%outputFile%
